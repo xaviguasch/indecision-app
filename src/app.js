77ce42aeab47b095ae17a548369877f33beecc78
xaviguasch <xaviguasch@gmail.com>
2018-10-23 16:39:34 +0200

    reassagning var to let/const

--- src/app.js
+++ src/app.js
@@ -3,13 +3,13 @@ console.log('App.js is running!');
 
 
 
-var app = {
+const app = {
     title: 'Indecision App',
     subtitle: 'Put your life in the hands of a computer',
     options: ['One', 'Two']
 }
 
-var template = (
+const template = (
     <div>
         <h1>{app.title}</h1>
         {app.subtitle && <p>{app.subtitle}</p>}
@@ -21,7 +21,7 @@ var template = (
     </div>
 )
 
-var user = {
+const user = {
     name: 'Mike',
     age: 26,
     location: 'Philadelphia'
@@ -33,7 +33,7 @@ function getLocation(location) {
     } 
 }
 
-var templateTwo = (
+const templateTwo = (
     <div>
         <h1>{user.name ? user.name : 'Anonymous'}</h1>
         {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}          
@@ -42,7 +42,7 @@ var templateTwo = (
 )
 
 
-var appRoot = document.getElementById('app')
+const appRoot = document.getElementById('app')
 
 
 
