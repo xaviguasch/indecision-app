ead93091e84ee6ecc89f698c0abc21240e4bd29f
xaviguasch <xaviguasch@gmail.com>
2018-10-24 15:50:21 +0200

    more es6 classes

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -12,15 +12,59 @@ class Person {
     }
 }
 
-const me = new Person('Xavi Guasch', 33)
-console.log(me.getDescription());
+class Student extends Person {
+    constructor(name, age, major) {
+        super(name, age)
+        this.major = major
+    }
+    hasMajor() {
+        return !!this.major   // converts a 'falsy' to false and a 'truthy' to true. undefined gets to false here.
+    }
+    getDescription() {
+        let description = super.getDescription()
+
+        if (this.hasMajor()) {
+            description += ` Their major is ${this.major}.`
+        }
+
+        return description        
+    } 
+}
+
+class Traveler extends Person {
+    constructor(name, age, homeLocation) {
+        super(name, age)
+        this.homeLocation = homeLocation
+    }
+    getGreeting() {
+        let greeting = super.getGreeting()
+
+        if (this.homeLocation) {
+            greeting += ` I am visiting from ${this.homeLocation}.`
+        }
+        
+        return greeting
+    }
+}
 
 
 
+const me = new Student('Xavi Guasch', 33, 'Economics')
+console.log(me)
+console.log(me.hasMajor())
+console.log(me.getDescription())
 
 
-const other = new Person()
-console.log(other.getDescription());
+const other = new Student()
+console.log(other);
+console.log(other.hasMajor())
+console.log(other.getDescription())
+
+
+const thatGuy = new Traveler('Laurie Palmer', 23, 'Singapour')
+console.log(thatGuy.getGreeting());
+
+
 
 
 
