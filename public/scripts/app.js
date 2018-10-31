a5157d4b911b617a27d761486baf0db4f1da029c
xaviguasch <xaviguasch@gmail.com>
2018-10-31 11:01:42 +0100

    added comments on handleDeleteOption

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -171,6 +171,11 @@ var Option = function Option(props) {
                 onClick: function onClick(e) {
                     props.handleDeleteOption(props.optionText);
                 }
+                //-onClick requires a reference to a function, 
+                //-so you need to pass it a function. . .
+                //-1. onClick={props.handleDeleteOption}  will not work because the handler needs the value of option
+                //-2. onClick={props.handleDeleteOption(props.optionText)} will not work because you are invoking the function not assigning it
+                //-3.The arrow function was created to solve the above two problems (a regular function would also have worked)
             },
             'remove'
         )
--- src/app.js
+++ src/app.js
@@ -121,6 +121,11 @@ const Option = (props) => {
             onClick={(e) => {
                 props.handleDeleteOption(props.optionText)
             }}
+            //-onClick requires a reference to a function, 
+            //-so you need to pass it a function. . .
+            //-1. onClick={props.handleDeleteOption}  will not work because the handler needs the value of option
+            //-2. onClick={props.handleDeleteOption(props.optionText)} will not work because you are invoking the function not assigning it
+            //-3.The arrow function was created to solve the above two problems (a regular function would also have worked)
             >
             remove
             </button>
