6063cb4ce98c10f103a6fa55c1baecb142ccd451
xaviguasch <xaviguasch@gmail.com>
2018-11-03 17:10:26 +0100

    styling button

--- src/components/Option.js
+++ src/components/Option.js
@@ -3,17 +3,18 @@ import React from 'react'
 const Option = (props) => (
     <div>
         {props.optionText}
-        <button 
-        onClick={(e) => {
-            props.handleDeleteOption(props.optionText)
-        }}
+        <button
+            className="button button--link"
+            onClick={(e) => {
+                props.handleDeleteOption(props.optionText)
+            }}
         //-onClick requires a reference to a function, 
         //-so you need to pass it a function. . .
         //-1. onClick={props.handleDeleteOption}  will not work because the handler needs the value of option
         //-2. onClick={props.handleDeleteOption(props.optionText)} will not work because you are invoking the function not assigning it
         //-3.The arrow function was created to solve the above two problems (a regular function would also have worked)
         >
-        remove
+            remove
         </button>
     </div>
 )
