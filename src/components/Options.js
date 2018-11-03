f1c8f0ec10da83a2d783e3a8d01075a13b78dd2e
xaviguasch <xaviguasch@gmail.com>
2018-11-03 18:06:16 +0100

    styling option item

--- src/components/Options.js
+++ src/components/Options.js
@@ -4,13 +4,18 @@ import Option from './Option'
 
 const Options = (props) => (
     <div>
-        <button 
-            onClick={props.handleDeleteOptions} 
-            className="button button--link"
-        >
-            Remove All
-        </button> 
-        {props.options.length === 0 && <p>Please add an option to get started</p>}             
+        <div className="widget-header">
+            <h3 className="widget-header__title">Your options</h3>
+            <button 
+                onClick={props.handleDeleteOptions} 
+                className="button button--link"
+                hidden={props.options.length === 0}
+            >
+                Remove All
+            </button> 
+        </div>
+
+        {props.options.length === 0 && <p className="widget__message">Please add an option to get started</p>}             
         {
             props.options.map((option) => (
                 <Option 
